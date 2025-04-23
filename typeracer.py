ccbb8a6070c5d115b19870e87f183cbaf8861554
Cyber0x3a <wl52hz@proton.me>
2025-04-23 08:16:53 +0430

    added some enhancements and the speed selection by user + support for higher speeds

--- typeracer.py
+++ typeracer.py
@@ -1,19 +1,25 @@
 import pyautogui
+import mouse
 from time import sleep
 from os import system
-print("")
-writing_text = input("Enter the copied text>")
-print("")
-print("")
-print("")
-input("Press Enter when the race started and click on the typeracer input field as fast as you can")
-sleep(2)
-writing_text_list = writing_text.split(" ")
-for i in writing_text_list:
-    for j in i:
-        pyautogui.press(j)
-    pyautogui.press(" ")
+
+while True:
+    speed = input("\nChoose the typing speed(Default 2): 1> ~300WPM, 2> ~200WPM, 3> ~100WPM")
+    if speed in ['1', '2', '3']:
+        if speed == '1':
+            speed = 0.3
+        elif speed == '2':
+            speed == 0.35
+        elif speed == '3':
+            speed == 0.5
+        break
+    if speed == '':
+        print('Selected the default speed')
+        speed = '2'
+
+writing_text = input("Enter the copied text> ")
+print("\n\nWaiting for left mouse click to start typing...")
+mouse.wait(button='left', target_types=('down',))
 system('cls')
-
-
-#add new features soon
+sleep(1)
+pyautogui.write(writing_text, interval=0.035)
